c5f926f7c2952b873d8ee7a1e9cc1043956228d5
Daniel Lamas <dlamash23@gmail.com>
2025-09-09 18:50:42 -0300

    primer commit

---
+++ src/test/java/com/ejercicioContador/Contador/ContadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicioContador.Contador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
